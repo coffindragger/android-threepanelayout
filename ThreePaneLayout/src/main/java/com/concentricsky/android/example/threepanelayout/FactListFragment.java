2f1ebf829ddae19878563865f099c5db593542f4
lithium <code@hlidskialf.com>
2013-09-12 21:58:52 -0700

    basic drawer navigation

---
+++ ThreePaneLayout/src/main/java/com/concentricsky/android/example/threepanelayout/FactListFragment.java
@@ -0,0 +1,67 @@
+package com.concentricsky.android.example.threepanelayout;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by wiggins on 9/12/13.
+ */
+public class FactListFragment extends android.support.v4.app.ListFragment
+{
+    private int[] mFactIds;
+    private ItemClickedListener mItemClickedListener;
+    private String[] mPlanetNames;
+    private int mPosition;
+
+    public interface ItemClickedListener
+    {
+        public void onItemClicked(int position, long id);
+    }
+
+    public FactListFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentActivity activity = getActivity();
+        try {
+            mItemClickedListener = (ItemClickedListener)activity;
+        } catch (ClassCastException e) { }
+
+        mFactIds = new int[] {R.array.mercury_items, R.array.venus_items, R.array.earth_items};
+        mPlanetNames = getResources().getStringArray(R.array.planets_array);
+
+        load_adapter();
+    }
+
+    public void setPlanet(int position)
+    {
+        mPosition = position;
+        load_adapter();
+    }
+
+    private void load_adapter()
+    {
+        FragmentActivity activity = getActivity();
+        if (activity != null && mPosition != -1) {
+
+            String[] facts = getResources().getStringArray(mFactIds[mPosition]);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, facts);
+            setListAdapter(adapter);
+            getActivity().getActionBar().setTitle(mPlanetNames[mPosition]);
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (mItemClickedListener != null) {
+            mItemClickedListener.onItemClicked(position, id);
+        }
+    }
+}
--- ThreePaneLayout/src/main/java/com/concentricsky/android/example/threepanelayout/MainActivity.java
+++ ThreePaneLayout/src/main/java/com/concentricsky/android/example/threepanelayout/MainActivity.java
@@ -1,17 +1,94 @@
 package com.concentricsky.android.example.threepanelayout;
 
+import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private boolean mIsTablet;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ArrayAdapter<String> mNavigationAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        mNavigationAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.planets_array));
+        mDrawerList = (ListView) findViewById(R.id.navigation_list);
+        mDrawerList.setAdapter(mNavigationAdapter);
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showFactsForPlanet(i);
+                if (mDrawerLayout != null) {
+                    mDrawerLayout.closeDrawers();
+                }
+            }
+        });
+
+
+
+
+
+        if (mDrawerLayout == null) {
+            mIsTablet = true;
+        } else {
+            mIsTablet = false;
+
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, android.R.string.ok, android.R.string.cancel);
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+            ActionBar ab = getActionBar();
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setHomeButtonEnabled(true);
+        }
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction trans = fragmentManager.beginTransaction();
+        FactListFragment frag = new FactListFragment();
+        frag.setPlanet(0);
+        trans.replace(R.id.list_frame, frag);
+        trans.commit();
+
     }
 
+    public void showFactsForPlanet(int pos) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction trans = fragmentManager.beginTransaction();
+        FactListFragment frag = new FactListFragment();
+        frag.setPlanet(pos);
+        trans.replace(R.id.list_frame, frag, "FactList");
+        trans.addToBackStack(null);
+        trans.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle != null && mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
